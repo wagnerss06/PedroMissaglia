c557e8cc86a7f44d39e07bdb05f4b0cacdbe73a2
PEDRO MISSAGLIA <pedro.missaglia@gmail.com>
2019-04-23 21:05:06 -0300

    ffdfd

---
+++ Alexandria.API/Controllers/BookcaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Alexandria.Model;
+using Alexandria.Model.DTO;
+using Alexandria.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json;
+
+
+namespace Alexandria.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class BookcaseController : ControllerBase
+    {
+
+
+
+    }
+}
+
--- Alexandria.Repository/BookcaseRepository.cs
+++ Alexandria.Repository/BookcaseRepository.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using Alexandria.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Alexandria.Repository
 {
-    class BookcaseRepository
+    public class BookcaseRepository : ICRUD<Bookcase>
     {
+
+        public void Add(Bookcase item)
+        {
+            using (Context context = new Context())
+            {
+                context.Bookcase.Add(item);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Bookcase bookId)
+        {
+            using (Context context = new Context())
+            {
+                context.Bookcase.Remove(bookId);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bookcase GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public List<Bookcase> GetItens()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, Bookcase item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
